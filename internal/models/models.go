34dd0b1527d92cf3eb83a2d8f57471f386b9e010
vishnu2321 <mandalavishnu750@gmail.com>
2025-08-06 22:36:49 +0530

    added DTO model of drawboardevent

--- internal/models/models.go
+++ internal/models/models.go
@@ -1,9 +1,45 @@
 package models
 
+import "time"
+
 // currently using as DTO but its better to keep models and DTOs seperate
 type User struct {
 	ID       string `json:"_id,omitempty"`
 	Name     string `json:"name,omitempty"`
 	Email    string `json:"email"`
 	Password string `json:"password"`
+}
+
+type DrawingEventDTO struct {
+	EventID   string    `bson:"eventId" json:"eventId"`
+	BoardID   string    `bson:"boardId" json:"boardId"`
+	UserID    string    `bson:"userId" json:"userId"`
+	Timestamp time.Time `bson:"timestamp" json:"timestamp"`
+	EventType string    `bson:"eventType" json:"eventType"`
+	Tool      string    `bson:"tool" json:"tool"`
+
+	// Common properties for all tools
+	Color     string `bson:"color,omitempty" json:"color,omitempty"`
+	Thickness int    `bson:"thickness,omitempty" json:"thickness,omitempty"`
+
+	// Position and size properties
+	X      float64 `bson:"x,omitempty" json:"x,omitempty"`
+	Y      float64 `bson:"y,omitempty" json:"y,omitempty"`
+	Width  float64 `bson:"width,omitempty" json:"width,omitempty"`
+	Height float64 `bson:"height,omitempty" json:"height,omitempty"`
+
+	// Points for freehand drawing and erasing
+	Points []Point `bson:"points,omitempty" json:"points,omitempty"`
+
+	// Text properties
+	Text     string `bson:"text,omitempty" json:"text,omitempty"`
+	FontSize int    `bson:"fontSize,omitempty" json:"fontSize,omitempty"`
+
+	// Object reference for deletion
+	ObjectID string `bson:"objectId,omitempty" json:"objectId,omitempty"`
+}
+
+type Point struct {
+	X float64 `bson:"x" json:"x"`
+	Y float64 `bson:"y" json:"y"`
 }
